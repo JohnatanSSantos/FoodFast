c4ed222fb4b7808d0b7471ae56cbd75f50b46080
JohnatanSSantos <johnatansantos317@gmail.com>
2022-10-27 22:01:41 -0300

    continuação menu

--- app/src/main/java/br/com/etecia/foodfast/MainActivity.java
+++ app/src/main/java/br/com/etecia/foodfast/MainActivity.java
@@ -1,8 +1,13 @@
 package br.com.etecia.foodfast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //inserindo o menu na barra de aplicativos
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+
+        //criando o método para clicar nos itens do menu
+
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mFav:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no favoritos",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mBuscar:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no favoritos",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mConfig:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no favoritos",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mHistorico:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no favoritos",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mSobre:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no favoritos",
+                        Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.mCompartilhar:
+                Toast.makeText(getApplicationContext(),
+                        "Cliquei no favoritos",
+                        Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
